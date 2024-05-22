c27c11251ab84d497ac00dcc58b042863e34c4aa
Enny J. Frick <enny.frick@conductorone.com>
2024-05-22 12:01:25 -0700

    add account observability resource (#1)
    * add account observability resource
    
    * add comments
    
    * go mod tidy
    
    * fix schema error
    
    * add example + generate docs
    
    * rename account_metrics resource to metrics_endpoint resource + refactor schema
    
    * fix metric endpoint test
    
    * Make account roles and namespace permissions case insensitive (#99)
    
    ## What was changed
    Make account roles and namespace permissions case insensitive, by sending and receiving it in lowercase.
    
    ## Why?
    The API currently returns capitalized roles/permissions, and we will be changing it to return lowercase roles/permissions as it should be case insensitive - and is when creating/updating users.
    
    ---------
    
    Co-authored-by: Travis <1060873+tminusplus@users.noreply.github.com>

--- internal/provider/namespace_search_attribute_resource.go
+++ internal/provider/namespace_search_attribute_resource.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/jpillora/maplock"
+
 	"github.com/temporalio/terraform-provider-temporalcloud/internal/client"
 
 	cloudservicev1 "github.com/temporalio/terraform-provider-temporalcloud/proto/go/temporal/api/cloud/cloudservice/v1"
@@ -51,7 +52,7 @@ func (r *namespaceSearchAttributeResource) Configure(_ context.Context, req reso
 		return
 	}
 
-	client, ok := req.ProviderData.(cloudservicev1.CloudServiceClient)
+	clientStore, ok := req.ProviderData.(client.ClientStore)
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected Data Source Configure Type",
@@ -61,7 +62,7 @@ func (r *namespaceSearchAttributeResource) Configure(_ context.Context, req reso
 		return
 	}
 
-	r.client = client
+	r.client = clientStore.CloudServiceClient()
 }
 
 func (r *namespaceSearchAttributeResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
